ec79db913e117bd7b95d698efc1893e68a1f22cb
tuan-nguyen-egany <tuan.nguyen@egany.com>
2019-12-31 10:23:25 +0700

    Updates

---
+++ precache-manifest.b011242bbe62826d07004d23d14be4e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88bc4c65aad06eb1a26fbf3feca077b",
+    "url": "//demo-transform/index.html"
+  },
+  {
+    "revision": "f17acfadcc803cf12cba",
+    "url": "//demo-transform/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "60a263c719fcfc4a9ccb",
+    "url": "//demo-transform/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f17acfadcc803cf12cba",
+    "url": "//demo-transform/static/js/2.c7089993.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "//demo-transform/static/js/2.c7089993.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60a263c719fcfc4a9ccb",
+    "url": "//demo-transform/static/js/main.35a262ae.chunk.js"
+  },
+  {
+    "revision": "20f84ec39cd078ad5611",
+    "url": "//demo-transform/static/js/runtime-main.f538b8b7.js"
+  }
+]);
